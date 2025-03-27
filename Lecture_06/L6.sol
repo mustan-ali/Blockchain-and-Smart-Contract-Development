a38a37af9722becd548a89cfde31d0594e3bdc22
Mustan Ali <74346675+mustan-ali@users.noreply.github.com>
2025-03-27 18:40:18 +0500

    add lecture 6 files

---
+++ Lecture_06/L6.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(address recipient) ERC20("MyToken", "MTK") {
+        _mint(recipient, 1000000 * 10**decimals());
+    }
+}
+
+contract Staking {
+
+    MyToken public token;
+    mapping(address => uint256) public stakedAmount;
+    mapping(address => uint256) public stakedTime;
+
+    constructor(address _token) {
+        token = MyToken(_token);
+    }
+
+    function stake(uint256 _amount) public {
+        stakedAmount[msg.sender] += _amount;
+        stakedTime[msg.sender] = block.timestamp;
+        token.transfer(address(this), _amount);
+    }
+
+    function unstake(uint256 _amount) public {
+        require(block.timestamp >= stakedTime[msg.sender] + 1 days, "You can only unstake after 1 day");
+
+        uint256 timeDiff = (block.timestamp - stakedTime[msg.sender]) / 1 days;
+
+        uint256 reward = (timeDiff * stakedAmount[msg.sender]) / 100;
+
+        stakedAmount[msg.sender] -= _amount;
+        stakedTime[msg.sender] = block.timestamp;
+
+        // token.transferFrom(address(this), msg.sender, reward + _amount);
+        token.transfer(msg.sender, _amount);
+    }
+}
---
+++ Lecture_06/README.md
@@ -0,0 +1,6 @@
+### Steps to Deploy & Run
+
+1. Deploy MyToken contract using user's address as the owner.
+2. Deploy Staking contract using contract address of MyToken.
+3. Call transfer function of MyToken contract to transfer tokens to the Staking contract address.
+4. Call stake function of Staking contract to stake tokens.
